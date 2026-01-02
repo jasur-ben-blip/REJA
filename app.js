b21757c81f52492780b7da927dcf04b1120fed55
Jasur(BEN) <abdujambulovj@gmail.com>
2026-01-02 20:17:00 +0900

    fix: Frontend JS

--- app.js
+++ app.js
@@ -36,12 +36,14 @@ app.post("/create-item", function (req, res) {
   console.log(req.body);
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("succesfully added");
-    }
+    // if (err) {
+    //   console.log(err);
+    //   res.end("something went wrong");
+    // } else {
+    //   res.end("succesfully added");
+    // }
+    console.log(data.ops);
+    res.json(data.ops[0]);
   });
 });
 
